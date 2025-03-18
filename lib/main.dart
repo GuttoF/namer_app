3f084426bd5c3ead6caccbebc35b2ff541494d0e
Gutto <guttolaudie@gmail.com>
2025-03-18 18:50:20 -0300

    feat: :lipstick: change text color

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,11 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(30.0),
-        child: Text(pair.asLowerCase),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
